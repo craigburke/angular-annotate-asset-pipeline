99f6537bf444139019e91e4143387d0e065f231c
Craig Burke <craig@craigburke.com>
2014-06-26 11:15:56 -0400

    Fixing plugin name and bumping version

--- AngularAnnotateAssetPipelineGrailsPlugin.groovy
+++ AngularAnnotateAssetPipelineGrailsPlugin.groovy
@@ -3,7 +3,7 @@ import com.craigburke.angular.AnnotateProcessor
 
 class AngularAnnotateAssetPipelineGrailsPlugin {
 
-    def version = "1.0.0"
+    def version = "1.0.1"
     def grailsVersion = "2.0 > *"
 
     def pluginExcludes = [
