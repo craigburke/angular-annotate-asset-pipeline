009d56b8c815fbf19556726efe67d9ec3d1a0671
Craig Burke <craig@craigburke.com>
2014-09-25 14:43:17 -0400

    Updating to ng-annotate v0.10.1

--- AngularAnnotateAssetPipelineGrailsPlugin.groovy
+++ AngularAnnotateAssetPipelineGrailsPlugin.groovy
@@ -4,7 +4,7 @@ import com.craigburke.angular.AnnotateProcessorUtil
 
 class AngularAnnotateAssetPipelineGrailsPlugin {
 
-    def version = "1.1.1"
+    def version = "1.1.2"
     def grailsVersion = "2.0 > *"
 
     def pluginExcludes = [
