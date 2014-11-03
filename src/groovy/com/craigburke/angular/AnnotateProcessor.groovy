25b33d528cbed9040fcb6c062fd8c1334d1585f2
Craig Burke <craig@craigburke.com>
2014-11-03 08:55:03 -0500

    Reverting to 48c38f8

--- src/groovy/com/craigburke/angular/AnnotateProcessor.groovy
+++ src/groovy/com/craigburke/angular/AnnotateProcessor.groovy
@@ -1,63 +1,53 @@
 package com.craigburke.angular
 
-import javax.script.ScriptEngine
-import javax.script.ScriptEngineManager
-
+import org.mozilla.javascript.Context
+import org.mozilla.javascript.Scriptable
 import org.springframework.core.io.ClassPathResource
 import asset.pipeline.AssetCompiler
 
 class AnnotateProcessor {
 
-	ScriptEngine engine
-	boolean useNashorn = true
-    def rhinoScope
+    Scriptable globalScope
+    ClassLoader classLoader
 
     AnnotateProcessor(AssetCompiler precompiler) {
-        def annotateResource = new ClassPathResource('com/craigburke/angular/ngannotate.js', this.class.classLoader)
-		def engineManager = new ScriptEngineManager()
-		
-		engine = engineManager.getEngineByName("nashorn")
-		
-		if (engine) {
-			println "Using Nashorn"
-			engine.eval(annotateResource.inputStream.text)
-		}
-		else { 
-			println "Using Rhino"
-			useNashorn = false
-			engine = engineManager.getEngineByName("rhino")
-			
-			def rhinoContext = Class.forName("org.mozilla.javascript.ContextFactory").getGlobal().enterContext()			
-	        rhinoScope = rhinoContext.initStandardObjects()
-	        rhinoContext.evaluateString(rhinoScope, annotateResource.inputStream.text, annotateResource.filename, 0, null)
-			try { 
-				rhinoContext.exit() 
-			}
-			catch (Exception e) { }				
-		}
-		
+    try {
+        classLoader = getClass().getClassLoader()
+        def annotateResource = new ClassPathResource('com/craigburke/angular/ngannotate.js', classLoader)
+
+        Context rhinoContext = Context.enter()
+        globalScope = rhinoContext.initStandardObjects()
+        rhinoContext.evaluateReader(globalScope, new InputStreamReader(annotateResource.inputStream, 'UTF-8'), annotateResource.filename, 0, null)
+    } catch (Exception e) {
+        throw new Exception("Annotation initialization failed.", e)
+    } finally {
+        try {
+            Context.exit()
+        } catch (IllegalStateException e) {}
+    }
     }
 
     def process(input, assetFile) {
         try {
-			def result
-			
-			if (useNashorn) {
-				result = engine.invokeFunction("ngAnnotate", input, [add: true])
-			}
-			else {
-				def context = Class.forName("org.mozilla.javascript.ContextFactory").getGlobal().enterContext()			
-				def annotateScope = context.newObject(rhinoScope)
-				
-				annotateScope.setParentScope(rhinoScope)
-				annotateScope.put("inputSrc", annotateScope, input)
+            def context = Context.enter()
+            def annotateScope = context.newObject(globalScope)
 
-				result = context.evaluateString(annotateScope, "ngAnnotate(inputSrc, {add: true})", "AngularJS annotate command", 0, null)
-			}
-			result.src
-        } 
-		catch (Exception e) {
+            annotateScope.setParentScope(globalScope)
+            annotateScope.put("inputSrc", annotateScope, input)
+
+            def result = context.evaluateString(annotateScope, "ngAnnotate(inputSrc, {add: true})", "AngularJS annotate command", 0, null)
+
+            if (result.containsKey('errors')) {
+                throw new Exception(result.errors.toString())
+            }
+            else {
+                return result.src
+            }
+
+        } catch (Exception e) {
             throw new Exception("AngularJS annotate failed", e)
+        } finally {
+            Context.exit()
         }
 
     }
