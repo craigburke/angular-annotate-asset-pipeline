474a30d3899439f7cfcd35fdf603b1761c7aad0e
Craig Burke <craig@craigburke.com>
2014-12-04 12:10:43 -0500

    Bumping to version 2.0.2

--- AngularAnnotateAssetPipelineGrailsPlugin.groovy
+++ AngularAnnotateAssetPipelineGrailsPlugin.groovy
@@ -1,5 +1,5 @@
 class AngularAnnotateAssetPipelineGrailsPlugin {
-    def version = "2.0.1"
+    def version = "2.0.2"
     def grailsVersion = "2.0 > *"
 
     def title = "AngularJS Annotate Asset-Pipeline Plugin"
