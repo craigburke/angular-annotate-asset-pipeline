a6f7ca3758490a053efbd1103831d430583339e3
Craig Burke <craig@craigburke.com>
2015-09-02 15:09:27 -0400

    Updating build script so that it produces a valid ngannotate.js file

--- core/src/main/groovy/com/craigburke/angular/AnnotateProcessor.groovy
+++ core/src/main/groovy/com/craigburke/angular/AnnotateProcessor.groovy
@@ -6,11 +6,10 @@ import asset.pipeline.AssetCompiler
 import com.craigburke.asset.JavaScriptProcessor
 import groovy.transform.CompileStatic
 
-@CompileStatic
 class AnnotateProcessor extends JavaScriptProcessor {
 
-    def process(String input, AssetFile assetFile) {
-        jsExec('ngannotate.js') {
+    String process(String input, AssetFile assetFile) {
+        javaScript('ngannotate.js') {
             Map result = ngAnnotate(input, [add: true, sourcemap: false, stats: false])      
             if (result.containsKey('errors')) {
                 throw new Exception(result.errors as String)
