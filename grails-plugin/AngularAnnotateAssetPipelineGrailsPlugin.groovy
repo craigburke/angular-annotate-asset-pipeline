48996cff84360de81f0b694b51f1abed284c68d1
Craig Burke <(none)>
2016-05-13 12:18:58 -0400

    Update ngAnnotate to 1.2.1. Bumping version to 2.4.1. Fixes #13

--- grails-plugin/AngularAnnotateAssetPipelineGrailsPlugin.groovy
+++ grails-plugin/AngularAnnotateAssetPipelineGrailsPlugin.groovy
@@ -1,5 +1,5 @@
 class AngularAnnotateAssetPipelineGrailsPlugin {
-    def version = "2.4.0"
+    def version = "2.4.1"
     def grailsVersion = "2.0 > *"
 
     def title = "AngularJS Annotate Asset-Pipeline Plugin"
